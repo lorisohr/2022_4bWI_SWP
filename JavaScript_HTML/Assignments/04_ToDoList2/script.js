18c5f1560fbde5b4f619d59136413e96bbaa117f
lorisohr <lyrigs@gmail.com>
2022-10-14 18:53:05 +0200

    Aufgabe ToDoList2 fertig

--- JavaScript_HTML/Assignments/04_ToDoList2/script.js
+++ JavaScript_HTML/Assignments/04_ToDoList2/script.js
@@ -1,7 +1,8 @@
 var task1 = {name: "putzen", isDone: false, responsible: "Loris"}
-var task2 = {name: "schwimmen", isDone: false, responsible: "Loris"}
+var task2 = {name: "schwimmen", isDone: true, responsible: "Loris"}
 
-const tasks = [task1, task2];
+const tasks = [];
+
 document.getElementById("button").addEventListener("click", function()
 {
     addTask();
@@ -17,11 +18,31 @@ function addTask()
     tasks.push(newTask);
 }
 
+function markTask(element)
+{ 
+    if(element.checked)
+    {
+        tasks[element.attributes["data-index"].value].isDone = true;
+    }
+    else
+    {
+        tasks[element.attributes["data-index"].value].isDone = false;
+    }
+}
+
 function getHTMLTasks() {
     let html = "";
+    let checked = "";
+    let index = 0;
     tasks.forEach(element =>
         {
-            html += "<li>" + element.name + " - " + element.responsible + "</li>"
+            checked = "";
+            if(element.isDone)
+            {
+                checked = "checked"
+            }
+            html += "<li><input onClick='markTask(this)' data-index='" + index + "' class='checkbox' type='checkbox' " + checked + "/>"+ element.name + " - " + element.responsible + "</li>"
+            index++
         });
         document.getElementById("list").innerHTML = html;
 }
